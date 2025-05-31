0d80b6747cabc5e4b3bc2425d24a1f51baa244a8
ShadowFax <55938330+ShadowFax1731@users.noreply.github.com>
2025-06-01 03:19:30 +0530

    Displaying toast notifs

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -40,6 +41,26 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 2500,
+          },
+          error: {
+            duration: 3500,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--colot-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
